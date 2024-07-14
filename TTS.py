1b50e1e3af282a288c86e8ee81ac59d7ca27bfc3
Ren Chen <93460298+3022249729@users.noreply.github.com>
2024-07-14 19:07:52 -0400

    Add files via upload

---
+++ TTS.py
@@ -0,0 +1,167 @@
+import random
+import string
+import discord
+from discord.ext import commands
+from gtts import gTTS
+import re
+import os
+import json
+import asyncio
+import time
+
+transitionWord = {
+    "en": "said",
+    "fr": "dit",
+    "zh-CN": "说",
+    "pt": "disse",
+    "es": "dijo"
+}
+
+class TTS(commands.Cog):
+    def reset(self):
+        self.queue = []
+        
+    def readSettings(self):
+        with open('serverSettings.json', 'r') as f:
+            self.settings = json.load(f)
+
+    def __init__(self, client):
+        self.client = client
+        self.queue = []
+        self.lastAuthor = None
+        self.lastAuthorTime = 0
+        self.readSettings()
+
+    async def autoLeave(self, ctx):
+        idleTime = 0
+        while ctx.voice_client:
+            if len(ctx.voice_client.channel.members) < 2:
+                await ctx.voice_client.disconnect()
+                self.reset()
+                break
+            
+            if not ctx.voice_client.is_playing():
+                idleTime += 3
+                if idleTime >= 300:
+                    await ctx.voice_client.disconnect()
+                    self.reset()
+                    break
+            else:
+                idleTime = 0
+            
+            await asyncio.sleep(3)
+        self.reset()
+
+    async def playTTS(self, ctx):
+        while self.queue:
+            ctx.voice_client.play(discord.FFmpegPCMAudio(f'./mp3files/{self.queue[0]}'), after=lambda e: self.client.loop.create_task(self.removePlayNext(ctx)))
+            self.lastAuthorTime = time.time()
+            while ctx.voice_client.is_playing():
+                await asyncio.sleep(1)
+
+    async def removePlayNext(self, ctx):
+        os.remove(f'./mp3files/{self.queue[0]}')
+        self.queue.pop(0)
+        if self.queue:
+            await self.playTTS(ctx)
+
+    @commands.command(name='setLanguage', aliases=['lang', 'sl'], description="Sets the language for TTS. \nSupported languages:\n`en`: English\n `fr`: French\n `zh-CN`: Chinese\n `pt`: Portuguese\n `es`: Spanish")
+    async def _setLanguage(self, ctx, lang):
+        if lang not in transitionWord.keys():
+            await ctx.send(f"Invalid language {lang}")
+            return
+        with open('serverSettings.json', 'r') as f:
+            settings = json.load(f)
+        settings[str(ctx.guild.id)]["language"] = lang
+        with open('serverSettings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
+        await ctx.send(f"Language set to {lang}")
+        self.settings = settings
+
+    @commands.command(name='setPrefix', aliases=['prefix', 'sp'], description="Change custom bot prefix.")
+    async def _setPrefix(self, ctx, prefix):
+        with open('serverSettings.json', 'r') as f:
+            settings = json.load(f)
+        settings[str(ctx.guild.id)]["prefix"] = prefix
+        with open('serverSettings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
+        await ctx.send(f"Prefix changed to {prefix}")
+        self.settings = settings
+
+    @commands.command(name='setChannel', aliases=['channel', 'sc'], description="Change the channel to monitor for TTS messages. Use this command in the targeted text chanel.")
+    async def _setChannel(self, ctx):
+        with open('serverSettings.json', 'r') as f:
+            settings = json.load(f)
+        channel = ctx.message.channel
+        settings[str(ctx.guild.id)]["channel"] = channel.id
+        with open('serverSettings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
+        await ctx.send(f"TTS channel set to #{channel.name}")
+        self.settings = settings
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        with open('serverSettings.json', 'r') as f:
+            settings = json.load(f)
+        settings.pop(str(guild.id))
+        with open('serverSettings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        with open('serverSettings.json', 'r') as f:
+            settings = json.load(f)
+
+        s = {"prefix": "!", "language": None, "channel": None}
+        
+        settings[str(guild.id)] = s
+        with open('serverSettings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
+            
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        global botInfo, transitionWord
+        try:
+            serverSetting = self.settings[str(message.guild.id)]
+        except KeyError:
+            return
+        
+        if message.channel.id == serverSetting["channel"] and not message.author.bot and not message.content.startswith(serverSetting["prefix"]):
+            ctx = await self.client.get_context(message)
+            if ctx.author.voice is None:
+                await ctx.send("Please join a voice channel first")        
+                return
+            elif ctx.voice_client is None:
+                voice_channel = ctx.author.voice.channel
+                await voice_channel.connect()
+                ctx.voice_client.stop()
+                self.client.loop.create_task(self.autoLeave(ctx))
+            elif ctx.voice_client.channel is not ctx.author.voice.channel:
+                await ctx.send("I'm already in a voice channel")
+                return
+
+            regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+            url = re.findall(regex, message.content)
+            if url:
+                return
+            # blocks url
+            
+            content = re.sub(r'<a?:\w+:\d+>', '', message.content)
+            if content == "":
+                return
+            # blocks emoji
+            
+            if time.time() - self.lastAuthorTime > 60 or self.lastAuthor is not ctx.author.display_name:
+                content = ctx.author.display_name + transitionWord[serverSetting["language"]] + "," + content
+
+            self.lastAuthor = ctx.author.display_name
+            tts = gTTS(text=content, lang=serverSetting["language"])
+            ttsFileName = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16)) + '.mp3'
+            tts.save(f"./mp3files/{ttsFileName}")
+            self.queue.append(ttsFileName)
+            
+            if not ctx.voice_client.is_playing():
+                await self.playTTS(ctx)
+
+async def setup(client):
+    await client.add_cog(TTS(client))
---
+++ main.py
@@ -0,0 +1,69 @@
+import discord
+import json
+from discord.ext import commands
+import TTS
+import asyncio
+import ctypes
+
+token = 'YOUR_TOKEN_HERE'
+
+cogs = [TTS]
+
+def get_prefix(client, message):
+    with open('serverSettings.json', 'r') as f:
+        settings = json.load(f)
+    return settings[str(message.guild.id)]["prefix"]
+
+
+client = commands.Bot(command_prefix=(get_prefix), case_insensitive=True, intents = discord.Intents.all())
+
+async def main():
+    for i in range(len(cogs)):  
+        await cogs[i].setup(client)
+
+
+class Help(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", color=0x47A7FF)
+        prefix = self.context.clean_prefix
+        embed.description = f"Do `{prefix}help <command>` for more help of the command.\nFor example: `{prefix}help setLanguage`\n\nCapitalization of the commands are ignored.\n`[argument]` are optional, `<argument>` are required.\n "
+        for cog, commands in mapping.items():
+            command_signatures = []
+
+            for c in commands:
+                signature = f'{prefix}`' + self.get_command_signature(c)[1:].replace(" ", '` ', 1)
+                command_signatures.append(signature)
+
+            cog_name = getattr(cog, "qualified_name", "No Category")
+            embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        embed.set_footer(text='TTS Bot by ren.xxx', icon_url=self.context.bot.user.avatar.url)
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command) , color=0x47A7FF)
+        if command.description:
+            embed.description = command.description
+        if alias := command.aliases:
+            alias = '`' + "`, `".join(alias) + '`'
+            embed.add_field(name="Aliases", value=alias, inline=False)
+
+        embed.set_footer(text='TTS Bot by ren.xxx', icon_url=self.context.bot.user.avatar.url)
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+client.help_command = Help()
+
+@client.event
+async def on_ready():
+    path = ctypes.util.find_library('libopus')
+    if path is None:
+        raise Exception("Opus not detected, please refer to README and install Opus before running the bot.")
+    discord.opus.load_opus(path)
+    
+asyncio.run(main())
+client.run(token)
